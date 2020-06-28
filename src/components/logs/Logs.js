dd9f4673891e2fb70e2049b1da0b4a6cb47029ef
NtombekaziSibetyu <sntombekazi@gmail.com>
2020-06-28 18:18:46 +0200

    last commit

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -18,7 +18,7 @@ const Logs = ({ log: { logs, loading }, getLogs }) => {
     }
     return (
         <ul className="collection with-header ">
-            <li className="collection-header purple accent-2">
+            <li className="collection-header green accent-3">
                 <h4 className="center">System Logs</h4>
                 {!loading && logs.length === 0 ? 
                 (<p className="center">No logs to show</p>) : 
